34c6ba1782910fb69d87146dacf256af35664e24
MaheswariSubburaj <maheswari.ksubburaj@gmail.com>
2021-07-28 13:44:25 +0000

    Deleted unused and change in people file.

--- src/components/people.js
+++ src/components/people.js
@@ -2,10 +2,19 @@ import { React } from 'react';
 
 const People = (people) => {
 	// eslint-disable-next-line react/destructuring-assignment
-	const count = people.length;
+	const { count } = people.length;
+	const { name } = people;
+	const { age } = people;
+	const { color } = people;
 
 	return <div>
-		people count:{count}
+		<div>
+			people count:{count}
+		</div>
+		style = { {
+			backgroundcolor: color,
+		}}
+		Hello! {name}, your age {age}
 	</div>;
 };
 
