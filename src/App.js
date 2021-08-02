674d5faecdac6364257e6a72e33cc4f71d35bfad
maheswari23 <maheswari.ksubburaj@gmail.com>
2021-08-02 08:14:36 -0600

    Number button created.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
-/* eslint-disable max-lines-per-function */
-/* eslint-disable no-mixed-spaces-and-tabs */
 import { React } from 'react';
-import './App.scss';
-import data from './components/data';
-import People from './components/people';
+import './App.css';
+// import data from './components/data.js';
+import NumberButton from './components/numberbutton';
+import numbers from './numbers.js';
+// import People from './components/people';
 
-const App = () => {
-	// eslint-disable-next-line no-unused-vars
-	const a = 0;
-	const { people } = data;
-
-	// eslint-disable-next-line no-console
-	console.log(data);
-
-	return <div className="App">
-		{People(people)}
-	</div>;
-};
+const App = () => <div className="App">
+	{numbers.map(NumberButton)}
+</div>;
 
 export default App;
