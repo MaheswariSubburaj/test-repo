44d5b87648677d0202e9d88c0c34030882bdb65e
maheswari23 <maheswari.ksubburaj@gmail.com>
2021-08-03 17:08:13 -0600

    Created symbol buttons.

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 import NumberButton from './components/numberbutton';
 import numbers from './numbers.js';
 import context from './core/context.js';
+import symbols from './symbols';
+import SymbolButton from './components/symbolbutton';
 
 const App = () => {
 	// eslint-disable-next-line no-console
 	console.log(context.state);
 
 	return <div className="App">
-		{numbers.map(NumberButton)}
+		<div>{numbers.map(NumberButton)}</div>
+		<div>{symbols.map(SymbolButton)}</div>
 	</div>;
 };
 
---
+++ src/components/symbolbutton.js
@@ -0,0 +1,7 @@
+import { React } from 'react';
+
+const SymbolButton = (symbol) => <div>
+	<button> { symbol } </button>
+</div>;
+
+export default SymbolButton;
