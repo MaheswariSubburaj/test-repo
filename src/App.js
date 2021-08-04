a64b3b80a88f5a10d653ac7953c969aaafb08186
MaheswariSubburaj <maheswari.ksubburaj@gmail.com>
2021-08-04 13:14:37 +0000

    Displayed count and num.

--- src/App.js
+++ src/App.js
@@ -6,14 +6,16 @@ import context from './core/context.js';
 import symbols from './symbols';
 import SymbolButton from './components/symbolbutton';
 
-const App = () => {
-	// eslint-disable-next-line no-console
-	console.log(context.state);
+const App = () =>
+// eslint-disable-next-line no-console
+// console.log(context.state);
 
-	return <div className="App">
+	<div className="App">
 		<div>{numbers.map(NumberButton)}</div>
 		<div>{symbols.map(SymbolButton)}</div>
-	</div>;
-};
+		<div>{context.state.count}</div>
+		<div>{context.state.num}</div>
+	</div>
+;
 
 export default App;
