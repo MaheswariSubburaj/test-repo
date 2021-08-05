8a51e967828e2df3d728c715f799d1e37cc72056
MaheswariSubburaj <maheswari.ksubburaj@gmail.com>
2021-08-05 13:40:55 +0000

    Created textbox.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import { React } from 'react';
 import './App.css';
-import numbers from './numbers.js';
-import context from './core/context.js';
-import symbols from './symbols';
-import SymbolButton from './components/symbolbutton';
-import NumberButtonRow from './components/numberbuttonrow';
+import TextBox from './components/input';
 
 const App = () =>
 // eslint-disable-next-line no-console
 // console.log(context.state);
 
-	<div className="App">
-		<div>{numbers.map((number) => number.map(NumberButtonRow))}</div>
-		<div>{symbols.map(SymbolButton)}</div>
-		<div>{context.state.count}</div>
-		<div>{context.state.num}</div>
-	</div>;
+	<div className="App">{ TextBox() } </div>;
 
 export default App;
---
+++ src/components/input.js
@@ -0,0 +1,8 @@
+import { React } from 'react';
+
+const TextBox = () =>
+	<div className="input">
+		<input type="text" size="15"/>
+	</div>;
+
+export default TextBox;
